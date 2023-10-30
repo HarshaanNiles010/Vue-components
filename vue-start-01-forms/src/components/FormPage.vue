3f1a232724c766a23d7fd6d47ce185d90916d62b
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-10-30 12:04:20 -0700

    it's not operational yet, working on fixing that

---
+++ vue-start-01-forms/src/components/FormPage.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container mb-3">
+        <form action="">
+            <div class="mb-3">
+                <label for="" class="form-label">
+                    Page Title
+                </label>
+                <input
+                type="text"
+                class="form-control"
+                :value="PageTitle"
+                />
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">
+                    Content
+                </label>
+                <textarea
+                type="text"
+                class="form-control"
+                rows="3"></textarea>
+            </div>
+            <div class="mb-3">
+                <button
+                class="btn btn-primary"
+                @click.prevent="PageCreated({PageTitle})"
+                >Create Page</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default{
+    props:['PageCreated'],
+    data() {
+        return {
+            PageTitle:''
+        }
+    }
+}
+
+</script>
