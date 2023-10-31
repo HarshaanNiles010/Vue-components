7a75f0be387df2b6fb3152b37aec3656b606cc2a
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-10-31 16:08:46 -0700

    geo location working

---
+++ me-n-eds-delivery/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Maps></Maps>
+</template>
+
+<script>
+import Maps from './components/Maps.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Maps
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ me-n-eds-delivery/src/components/Maps.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+    <h1>Geolocation Finder</h1>
+    <button @click="getLocation">Get My Location</button>
+        <div v-if="location">
+            <p>Latitude: {{ location.latitude }}</p>
+            <p>Longitude: {{ location.longitude }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    Name:'Maps',
+    data() {
+        return {
+            location: null
+        };
+    },
+        methods: {
+        getLocation() {
+            if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(position => {
+                this.location = {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+                };
+            }, error => {
+                console.error("Error getting location:", error);
+            });
+            } else {
+            console.error("Geolocation is not supported by your browser.");
+            }
+        }
+        }
+    };
+</script>
+    
