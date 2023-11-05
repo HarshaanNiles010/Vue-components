bc03e9c4e8af89345ca94dad51fc75e267e29d30
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-11-05 11:27:34 -0800

    form submission and list is working

--- me-n-eds-delivery/src/components/NewFormList.vue
+++ me-n-eds-delivery/src/components/NewFormList.vue
@@ -1,35 +1,52 @@
 <template>
-    <div>
-        <h1>User Input List</h1>
-        <input v-model="newItem" @keydown.enter="addItem" placeholder="Enter an item" />
-        <button @click="addItem">Add</button>
-        <ul>
-            <li v-for="(item, index) in items" :key="index">
-                {{ item }}
-                <button @click="removeItem(index)">Remove</button>
-            </li>
-        </ul>
+    <div v-show="inputField">
+        <input type="email" v-model="email"/>
+        <button @click="submitEmail">Submit</button>
+    </div>
+
+    <div v-show="showListBool">
+        <li v-for="(item,index) in items" :key="index">
+            {{ item.email }}
+        </li>
+        <div v-show="removeEmailBtn">
+            <button @click="removeEmail">Remove</button>
+        </div>
+    </div>
+    <div v-show="EmptyList">
+        <h1>The list is empty, add another entry for it to show up in the list</h1>
     </div>
 </template>
 
-<script>
-export default {
-    data() {
-        return {
-            newItem: '',
-            items: []
-        };
-    },
-    methods: {
-        addItem() {
-        if (this.newItem) {
-            this.items.push(this.newItem);
-            this.newItem = '';
+<script setup>
+    import {ref} from 'vue';
+    
+    const inputField = ref(true);
+    const showListBool = ref(false);
+    const EmptyList = ref(false);
+    const removeEmailBtn = ref(true);
+
+    const email = ref('Hi@abc.com');
+    const parentMessage = ref('Parent');
+    const items = ref(
+        [{email: 'Hi@abc.com'},
+        {email: 'Bye@abc.com'}]
+    )
+    function submitEmail(){
+        inputField.value = false;
+        showListBool.value = true;
+        removeEmailBtn.value = true;
+        EmptyList.value = false;
+        items.value.push({email: email.value})
+    }
+    function removeEmail(){
+        if (items.value.length > 0){
+            items.value.pop();
         }
-    },
-        removeItem(index) {
-            this.items.splice(index, 1);
+        else{
+            removeEmailBtn.value = false;
+            inputField.value = true;
+            EmptyList.value = true;
         }
     }
-};
+
 </script>
